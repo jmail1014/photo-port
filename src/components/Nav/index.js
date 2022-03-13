dd4af561bfab80c6351f1692ad42432de2c677d0
jclong <j.mail1014@gmail.com>
2022-03-13 12:48:21 -0400

    create tests for About component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
   const categories = [
@@ -20,31 +21,39 @@ function Nav() {
       description: "Fields, farmhouses, waterfalls, and the beauty of nature",
     },
   ];
-  function categorySelected(name) {
-    console.log(`${name} clicked`);
-  }
+  const handleClick = () => {
+    console.log("click handled");
+  };
+
   return (
-    <header>
+    <header data-testid="header" className="flex-row px-1">
       <h2>
         <a href="/">
           <span role="img" aria-label="camera">
+            {" "}
             📸
-          </span>
+          </span>{" "}
           Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
-          <li className="mx2">
-            <a href="#about">About me</a>
+          <li className="mx-2">
+            <a href="#about" onClick={() => handleClick()}>
+              About me
+            </a>
           </li>
-          <li>
-            <span>Contact</span>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick()}>Contact</span>
           </li>
           {categories.map((category) => (
             <li className="mx-1" key={category.name}>
-              <span onClick={categorySelected(category.name)}>
-                {category.name}
+              <span
+                onClick={() => {
+                  handleClick();
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
               </span>
             </li>
           ))}
