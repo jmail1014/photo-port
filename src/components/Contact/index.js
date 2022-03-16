dbe7b8a438575d15e9f6258fd17f67f1b52365dd
jclong <j.mail1014@gmail.com>
2022-03-16 16:03:28 -0400

    add tests in Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
   const [formState, setFormState] = useState({
@@ -7,27 +9,45 @@ function ContactForm() {
     message: "",
   });
 
-  function handleChange(e) {
-    setFormState({ ...formState, [e.target.name]: e.target.value });
+  const [errorMessage, setErrorMessage] = useState("");
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log("Form", formState);
+    }
   };
 
-  function handleSubmit(e) {
-      e.preventDefault();
-      console.log(formState)
-  }
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+  };
 
-  // JSX
   return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
           <input
             type="text"
             name="name"
-            defaultValue={formState.name}
-            onChange={handleChange}
+            defaultValue={name}
+            onBlur={handleChange}
           />
         </div>
         <div>
@@ -35,8 +55,8 @@ function ContactForm() {
           <input
             type="email"
             name="email"
-            defaultValue={formState.email}
-            onChange={handleChange}
+            defaultValue={email}
+            onBlur={handleChange}
           />
         </div>
         <div>
@@ -44,11 +64,18 @@ function ContactForm() {
           <textarea
             name="message"
             rows="5"
-            defaultValue={formState.message}
-            onChange={handleChange}
+            defaultValue={message}
+            onBlur={handleChange}
           />
         </div>
-        <button type="submit">Submit</button>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">
+          Submit
+        </button>
       </form>
     </section>
   );
